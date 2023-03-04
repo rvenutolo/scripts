6eccf8dae530f1f675144ce649c0e33c724726e2
Rick Venutolo <rick@venutolo.org>
2023-03-04 14:59:10 -0600

    update ipv4_to_num and add num_to_ipv4 funcs

--- lib/functions.bash
+++ lib/functions.bash
@@ -288,9 +288,17 @@ function move_file() {
   log "Moved: $1 -> $2"
 }
 
+# $1 = ip
 function ipv4_to_num() {
-  IFS=. read -r a b c d <<< "$*"
-  printf '%d\n' "$((a * 256 ** 3 + b * 256 ** 2 + c * 256 + d))"
+  check_exactly_1_arg "$@"
+  IFS=. read -r a b c d <<< "$1"
+  echo "$(((a << 24) + (b << 16) + (c << 8) + d))"
+}
+
+# $1 = ip
+function num_to_ipv4() {
+  check_exactly_1_arg "$@"
+  echo "$(($1 >> 24 & 0xff)).$(($1 >> 16 & 0xff)).$(($1 >> 8 & 0xff)).$(($1 & 0xff))"
 }
 
 # path_remove "$(this_script_dir)"
