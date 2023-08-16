fe2c2643c8e8a0adf9ca92223f8f96c8e3a44cc3
Rick Venutolo <rick@venutolo.org>
2023-08-15 21:53:53 -0400

    move package stuff to root script

--- misc/set-up-linode-ubuntu-root.sh
+++ misc/set-up-linode-ubuntu-root.sh
@@ -24,8 +24,34 @@ if ! id --user 'rvenutolo' > /dev/null 2>&1; then
   until passwd 'rvenutolo'; do :; done
 fi
 
-log 'Updating packages'
-apt-get update
+log 'Adding Docker to package sources'
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+sudo install --mode='0755' --directory '/etc/apt/keyrings'
+curl --disable --fail --silent --location --show-error 'https://download.docker.com/linux/ubuntu/gpg' | sudo gpg --dearmor -o '/etc/apt/keyrings/docker.gpg'
+sudo chmod a+r '/etc/apt/keyrings/docker.gpg'
+# shellcheck disable=SC1091
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(source /etc/os-release && echo "${VERSION_CODENAME}") stable" | sudo tee '/etc/apt/sources.list.d/docker.list' > /dev/null
+
+log 'Installing apt packages'
+sudo apt-get update
+sudo apt-get install --yes \
+  age \
+  ca-certificates \
+  curl \
+  docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
+  fail2ban \
+  git \
+  gnupg \
+  nala \
+  nano \
+  micro \
+  openssh-client openssh-server \
+  plocate \
+  ufw \
+  unzip \
+  wget
+
+log 'Running apt dist-upgrade'
 apt-get dist-upgrade --yes
 
 log 'Done -- Rebooting'
