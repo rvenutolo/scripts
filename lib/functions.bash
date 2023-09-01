cc91a293dab2449ec410bb1bf3057ddc74ee5c0b
Rick Venutolo <rick@venutolo.org>
2023-09-01 03:50:00 -0400

    consolidate functions files

--- lib/functions.bash
+++ lib/functions.bash
@@ -273,3 +273,283 @@ function this_script_dir() {
   check_no_args "$@"
   cd -- "$(dirname -- "${BASH_SOURCE[1]}")" &> /dev/null && pwd
 }
+
+function auto_answer() {
+  [[ -n "${SCRIPTS_AUTO_ANSWER:-}" ]]
+}
+
+# $1 = question
+function prompt_ny() {
+  check_exactly_1_arg "$@"
+  REPLY=''
+  if auto_answer; then
+    REPLY='n'
+  fi
+  while [[ "${REPLY}" != 'y' && "${REPLY}" != 'n' ]]; do
+    read -rp "$1 [y/N]: "
+    if [[ ${REPLY} == [yY] ]]; then
+      REPLY='y'
+    elif [[ "${REPLY}" == '' || "${REPLY}" == [nN] ]]; then
+      REPLY='n'
+    fi
+  done
+  [[ "${REPLY}" == 'y' ]]
+}
+
+# $1 = question
+function prompt_yn() {
+  check_exactly_1_arg "$@"
+  REPLY=''
+  if auto_answer; then
+    REPLY='y'
+  fi
+  while [[ "${REPLY}" != 'y' && "${REPLY}" != 'n' ]]; do
+    read -rp "$1 [Y/n]: "
+    if [[ "${REPLY}" == '' || ${REPLY} == [yY] ]]; then
+      REPLY='y'
+    elif [[ "${REPLY}" == [nN] ]]; then
+      REPLY='n'
+    fi
+  done
+  [[ "${REPLY}" == 'y' ]]
+}
+
+# $1 = question
+# $2 = default value (optional)
+function prompt_for_value() {
+  check_at_least_1_arg "$@"
+  check_at_most_2_args "$@"
+  if [[ -n "${2:-}" ]]; then
+    REPLY=''
+    if auto_answer; then
+      REPLY="$2"
+    fi
+    if [[ "${REPLY}" == '' ]]; then
+      read -rp "$1 [$2]: "
+      if [[ "${REPLY}" == '' ]]; then
+        REPLY="$2"
+      fi
+    fi
+    echo "${REPLY}"
+  else
+    REPLY=''
+    while [[ -z "${REPLY}" ]]; do
+      read -rp "$1: "
+    done
+    echo "${REPLY}"
+  fi
+}
+
+# $1 = target file
+# $2 = link file
+function link_user_file() {
+  check_not_root
+  check_exactly_2_args "$@"
+  if [[ ! -f "$1" ]]; then
+    die "$1 does not exist"
+  fi
+  if [[ -L "$2" && "$(readlink --canonicalize "$2")" == "$(readlink --canonicalize "$1")" ]]; then
+    exit 0
+  fi
+  if [[ -f "$2" ]]; then
+    diff --color --unified "$2" "$1" || true
+    if ! prompt_yn "$2 exists - Link: $1 -> $2?"; then
+      exit 0
+    fi
+  else
+    if ! prompt_yn "Link: $1 -> $2?"; then
+      exit 0
+    fi
+  fi
+  log "Linking: $1 -> $2"
+  mkdir --parents "$(dirname "$2")"
+  ln --symbolic --force "$1" "$2"
+  log "Linked: $1 -> $2"
+}
+
+# $1 = old file location
+# $2 = new file location
+function move_user_file() {
+  check_not_root
+  check_exactly_2_args "$@"
+  if [[ ! -f "$1" ]]; then
+    die "$1 does not exist"
+  fi
+  if [[ "$1" == "$2" ]]; then
+    die "File paths are the same"
+  fi
+  if [[ -f "$2" ]]; then
+    diff --color --unified "$2" "$1" || true
+    if ! prompt_yn "$2 exists - Overwrite: $1 -> $2?"; then
+      exit 0
+    fi
+  else
+    if ! prompt_yn "Move $1 -> $2?"; then
+      exit 0
+    fi
+  fi
+  log "Moving: $1 -> $2"
+  mkdir --parents "$(dirname "$2")"
+  mv "$1" "$2"
+  log "Moved: $1 -> $2"
+}
+
+# $1 = old file location
+# $2 = new file location
+function copy_user_file() {
+  check_not_root
+  check_exactly_2_args "$@"
+  if [[ ! -f "$1" ]]; then
+    die "$1 does not exist"
+  fi
+  if [[ "$1" == "$2" ]]; then
+    die "File paths are the same"
+  fi
+  if [[ -f "$2" ]]; then
+    if cmp --silent "$1" "$2"; then
+      exit 0
+    else
+      diff --color --unified "$2" "$1" || true
+      if ! prompt_yn "$2 exists - Overwrite: $1 -> $2?"; then
+        exit 0
+      fi
+    fi
+  else
+    if ! prompt_yn "Copy $1 -> $2?"; then
+      exit 0
+    fi
+  fi
+  log "Copying: $1 -> $2"
+  mkdir --parents "$(dirname "$2")"
+  cp "$1" "$2"
+  log "Copied: $1 -> $2"
+}
+
+# $1 = source file
+# $2 = destination file
+function copy_system_file() {
+  check_exactly_2_args "$@"
+  if [[ ! -f "$1" ]]; then
+    die "$1 does not exist"
+  fi
+  if [[ "$1" == "$2" ]]; then
+    die "File paths are the same"
+  fi
+  if [[ -f "$2" ]]; then
+    if cmp --silent "$1" "$2"; then
+      exit 0
+    else
+      diff --color --unified "$2" "$1" || true
+      if ! prompt_yn "$2 exists - Overwrite: $1 -> $2?"; then
+        exit 0
+      fi
+    fi
+  else
+    if ! prompt_yn "Copy $1 -> $2?"; then
+      exit 0
+    fi
+  fi
+  log "Copying: $1 -> $2"
+  sudo mkdir --parents "$(dirname "$2")"
+  sudo cp "$1" "$2"
+  log "Copied: $1 -> $2"
+}
+
+# $1 = service unit file
+function user_service_unit_file_exists() {
+  systemctl --user list-unit-files --all --quiet "$1" > /dev/null
+}
+
+# $1 = service unit file
+function enable_user_service_unit() {
+  check_not_root
+  check_exactly_1_arg "$@"
+  if user_service_unit_file_exists "$1"; then
+    if ! systemctl is-enabled --user --quiet "$1" && prompt_yn "Enable and start $1 user service?"; then
+      log "Enabling and starting $1 user service"
+      systemctl enable --now --user --quiet "$1"
+      log "Enabled and started $1 user service"
+    fi
+  else
+    log "User service unit files does not exist: $1"
+  fi
+}
+
+# $1 = service unit file
+function restart_user_service_if_enabled() {
+  check_not_root
+  check_exactly_1_arg "$@"
+  if user_service_unit_file_exists "$1"; then
+    if systemctl is-enabled --user --quiet "$1" && prompt_yn "Restart $1 user service?"; then
+      log "Restarting $1 user service"
+      systemctl restart --user --quiet "$1"
+      log "Restarted $1 user service"
+    fi
+  else
+    log "User service unit files does not exist: $1"
+  fi
+}
+
+# $1 = service unit file
+function system_service_unit_file_exists() {
+  systemctl --system list-unit-files --all --quiet "$1" > /dev/null
+}
+
+# $1 = service unit file
+function enable_system_service_unit() {
+  check_exactly_1_arg "$@"
+  if system_service_unit_file_exists "$1"; then
+    if ! systemctl is-enabled --system --quiet "$1" && prompt_yn "Enable and start $1 system service?"; then
+      log "Enabling and starting $1 system service"
+      sudo systemctl enable --now --system --quiet "$1"
+      log "Enabled and started $1 system service"
+    fi
+  else
+    log "System service unit files does not exist: $1"
+  fi
+}
+
+# $1 = service unit file
+function restart_system_service_if_enabled() {
+  check_exactly_1_arg "$@"
+  if system_service_unit_file_exists "$1"; then
+    if systemctl is-enabled --system --quiet "$1" && prompt_yn "Restart $1 system service?"; then
+      log "Restarting $1 system service"
+      sudo systemctl restart --system --quiet "$1"
+      log "Restarted $1 system service"
+    fi
+  else
+    log "System service unit files does not exist: $1"
+  fi
+}
+
+function reload_sysctl_conf() {
+  check_no_args "$@"
+  if prompt_yn 'Reload sysctl configuration?'; then
+    log 'Reloading sysctl configuration'
+    sudo sysctl --system --quiet
+    log 'Reloaded sysctl configuration'
+  fi
+}
+
+# $1 = packages list type (cargo flatpaks nixpkgs snaps)
+function get_packages_list() {
+  check_exactly_1_arg "$@"
+  case "$1" in
+    cargo | flatpaks | nixpkgs | snaps | sdkman) : ;;
+    *) die "Unexpected package list type: $1" ;;
+  esac
+  local package_list_url="https://raw.githubusercontent.com/rvenutolo/packages/main/$1.csv"
+  if is_personal && is_desktop; then
+    local package_list_column=3
+  elif is_personal && is_laptop; then
+    local package_list_column=4
+  elif is_work && is_laptop; then
+    local package_list_column=5
+  elif is_server; then
+    local package_list_column=6
+  else
+    die "Could not determine which computer this is"
+  fi
+  download "${package_list_url}" | awk -F ',' -v "col_num=${package_list_column}" '$col_num == "y" && $7 == "" { print $2 }'
+}
