e49ce8354fe932e81970a97972342ff0ce1cd2ba
Rick Venutolo <rick@venutolo.org>
2024-05-12 18:42:55 -0500

    add some os functions

--- functions.bash
+++ functions.bash
@@ -175,6 +175,18 @@ function os_id() {
   grep --only-matching --perl-regexp '^ID=\K\w+$' '/etc/os-release'
 }
 
+#shellcheck disable=SC2120
+function os_codename() {
+  check_no_args "$@"
+  grep --only-matching --perl-regexp '^VERSION_CODENAME=\K\w+$' '/etc/os-release'
+}
+
+#shellcheck disable=SC2120
+function os_arch() {
+  check_no_args "$@"
+  dpkg --print-architecture
+}
+
 function is_arch() {
   check_no_args "$@"
   [[ "$(os_id)" == 'arch' ]]
