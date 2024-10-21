eb3e8df16904c8573a3360867dfc90883c1c5b90
Rick Venutolo <rick@venutolo.org>
2024-10-21 16:29:45 -0400

    rework contains functions

--- functions.bash
+++ functions.bash
@@ -141,22 +141,69 @@ function path_prepend() {
   path_remove "$1" && PATH="$1:$PATH"
 }
 
-# expected to pipe to this function, ex: groups | contains_word 'wheel'
+
+# expected to pipe to this function, ex: echo 'foobar' | contains_exactly 'ooba'
+# $1 = string
+function contains_exactly() {
+  check_exactly_1_arg "$@"
+  check_for_stdin
+  grep --quiet --fixed-strings "$1"
+}
+
+# expected to pipe to this function, ex: echo 'FOOBAR' | contains_exactly_ignore_case 'ooba'
+# $1 = string
+function contains_exactly_ignore_case() {
+  check_exactly_1_arg "$@"
+  check_for_stdin
+  grep --quiet --fixed-strings --ignore-case "$1"
+}
+
+# expected to pipe to this function, ex: echo 'foobar' | contains_regex '^foo'
+# $1 = string
+function contains_regex() {
+  check_exactly_1_arg "$@"
+  check_for_stdin
+  grep --quiet "$1"
+}
+
+# expected to pipe to this function, ex: echo 'FOOBAR' | contains_regex_ignore_case '^foo'
+# $1 = string
+function contains_regex_ignore_case() {
+  check_exactly_1_arg "$@"
+  check_for_stdin
+  grep --quiet --ignore-case "$1"
+}
+
+# expected to pipe to this function, ex: echo 'foo bar baz' | contains_word 'bar'
 # $1 = word
 function contains_word() {
-  # expected to pipe to this function
+  check_exactly_1_arg "$@"
+  check_for_stdin
+  grep --quiet --fixed-strings --word-regex "$1"
+}
+
+# expected to pipe to this function, ex: echo 'FOO BAR BAZ' | contains_word_ignore_case 'bar'
+# $1 = word
+function contains_word_ignore_case() {
   check_exactly_1_arg "$@"
   check_for_stdin
   grep --quiet --fixed-strings --ignore-case --word-regex "$1"
 }
 
-# expected to pipe to this function, ex: echo 'foo' | contains 'f'
-# $1 = string
-function contains() {
-  # expected to pipe to this function
+# expected to pipe to this function, ex: echo 'foo bar baz' | contains_word_regex '^foo'
+# $1 = word
+function contains_word_regex() {
   check_exactly_1_arg "$@"
   check_for_stdin
-  grep --quiet --fixed-strings --ignore-case "$1"
+  grep --quiet --word-regex "$1"
+}
+
+# expected to pipe to this function, ex: echo 'FOO BAR BAZ' | contains_word_regex_ignore_case '^foo'
+# $1 = word
+function contains_word_regex_ignore_case() {
+  check_exactly_1_arg "$@"
+  check_for_stdin
+  grep --quiet --ignore-case --word-regex "$1"
 }
 
 # $1 = file
@@ -164,6 +211,55 @@ function contains() {
 function file_contains_exactly() {
   check_exactly_2_args "$@"
   grep --quiet --fixed-strings "$2" "$1"
+}
+
+# $1 = file
+# $2 = string
+function file_contains_exactly_ignore_case() {
+  check_exactly_2_args "$@"
+  grep --quiet --fixed-strings --ignore-case "$2" "$1"
+}
+
+# $1 = file
+# $2 = string
+function file_contains_regex() {
+  check_exactly_2_args "$@"
+  grep --quiet "$2" "$1"
+}
+
+# $1 = file
+# $2 = string
+function file_contains_regex_ignore_case() {
+  check_exactly_2_args "$@"
+  grep --quiet --ignore-case "$2" "$1"
+}
+
+# $1 = file
+# $2 = string
+function file_contains_word() {
+  check_exactly_2_args "$@"
+  grep --quiet --fixed-strings --word-regex "$2" "$1"
+}
+
+# $1 = file
+# $2 = string
+function file_contains_word_ignore_case() {
+  check_exactly_2_args "$@"
+  grep --quiet --fixed-strings --ignore-case --word-regex "$2" "$1"
+}
+
+# $1 = file
+# $2 = string
+function file_contains_word_regex() {
+  check_exactly_2_args "$@"
+  grep --quiet --word-regex "$2" "$1"
+}
+
+# $1 = file
+# $2 = string
+function file_contains_word_regex_ignore_case() {
+  check_exactly_2_args "$@"
+  grep --quiet --ignore-case --word-regex "$2" "$1"
 }
 
 function array_to_lines() {
@@ -261,7 +357,7 @@ function is_tumbleweed() {
 # $1 = env
 function is_desktop_env() {
   check_exactly_1_arg "$@"
-  echo "${XDG_CURRENT_DESKTOP:-}" | contains_word "$1"
+  echo "${XDG_CURRENT_DESKTOP:-}" | contains_word_ignore_case "$1"
 }
 
 function is_kde() {
