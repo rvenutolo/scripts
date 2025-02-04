2805f7e0f3fe71c07d1526966ed06d1bc4bdb850
Rick Venutolo <rick@venutolo.org>
2025-02-04 17:07:17 -0500

    add ensure_trailing_slash

--- functions.bash
+++ functions.bash
@@ -159,6 +159,19 @@ function path_prepend() {
   path_remove "$1" && PATH="$1:$PATH"
 }
 
+# $1 = string
+function ensure_trailing_slash()
+  check_exactly_1_arg "$@"
+  if [[ -n "$1" ]]; then
+    case "$1" in
+      */) echo "$1" ;;
+      *) echo "$1/" ;;
+    esac
+  else
+    echo "$1"
+  fi
+}
+
 # expected to pipe to this function, ex: echo 'foobar' | contains_exactly 'ooba'
 # $1 = string
 function contains_exactly() {
