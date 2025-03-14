eaf968de91324754e9f00765b4a11f68f48adb9a
Rick Venutolo <rick.venutolo@gmail.com>
2025-03-14 00:00:41 -0400

    add generate_password

---
+++ functions/passwords.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# $1 = length
+function generate_password() {
+  check_at_most_1_arg "$@"
+  pwgen --secure --capitalize --numerals --symbols --remove-chars '$\\:;=`"'\' --num-passwords 1 "${1:-32}"
+}
