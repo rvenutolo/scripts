37781460b4a9ed13ae81ebe3d3da276ab01b72d1
Rick Venutolo <rick.venutolo@gmail.com>
2025-03-13 22:06:36 -0400

    add log_with_date

--- functions/log.bash
+++ functions/log.bash
@@ -4,6 +4,10 @@ function log() {
   echo -e "\033[0;32m[$(date +%T) ${0##*/}] $*\033[0m" >&2
 }
 
+function log_with_date() {
+  echo -e "\033[0;32m[$(date '+%Y-%m-%d %T') ${0##*/}] $*\033[0m" >&2
+}
+
 function die() {
   echo -e "\033[0;31mDIE: $* (at ${BASH_SOURCE[1]}:${FUNCNAME[1]} line ${BASH_LINENO[0]})\033[0m" >&2
   exit 1
