219f2ef601d888a682eceda44d1c816d887e9c94
Rick Venutolo <rick.venutolo@gmail.com>
2025-03-13 23:46:41 -0400

    add dir_exists

--- functions/dirs.bash
+++ functions/dirs.bash
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+# $1 = dir
+function dir_exists() {
+  check_exactly_1_arg "$@"
+  [[ -d "$1" ]]
+}
+
 # $@ = targets
 function create_dir() {
   check_at_least_1_arg "$@"
   for target in "$@"; do
-    if [[ ! -d "${target}" ]]; then
+    if ! dir_exists "${target}"; then
       log "Creating ${target}"
       mkdir --parents "${target}"
       log "Created ${target}"
@@ -16,7 +22,7 @@ function create_dir() {
 function root_create_dir() {
   check_at_least_1_arg "$@"
   for target in "$@"; do
-    if [[ ! -d "${target}" ]]; then
+    if ! dir_exists "${target}"; then
       log "Creating ${target}"
       sudo mkdir --parents "${target}"
       log "Created ${target}"
