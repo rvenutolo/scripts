9ad913761da6691b4991a508601f807ebee61cdc
Rick Venutolo <rick@venutolo.org>
2025-03-15 23:17:13 -0400

    add more characters to avoid

--- functions/passwords.bash
+++ functions/passwords.bash
@@ -3,5 +3,5 @@
 # $1 = length
 function generate_password() {
   check_at_most_1_arg "$@"
-  pwgen --secure --capitalize --numerals --symbols --remove-chars '$\\/:;=`"'\' --num-passwords 1 "${1:-32}"
+  pwgen --secure --capitalize --numerals --symbols --remove-chars '|&$\\/:;=`"'\' --num-passwords 1 "${1:-32}"
 }
