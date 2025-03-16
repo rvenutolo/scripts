f32b6a6707b81bfc0643fa3c5111e1ae8242428b
Rick Venutolo <rick.venutolo@gmail.com>
2025-03-16 01:01:21 -0400

    rename func

--- functions/env.bash
+++ functions/env.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # $@ = variable names
-function check_env_var_set() {
+function assert_var_set() {
   check_at_least_1_arg "$@"
   for var in "$@"; do
     if [[ -z "${!var:-}" ]]; then
