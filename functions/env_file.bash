7013551c05c3b6baa5e89821351e619c00b19843
Rick Venutolo <rick.venutolo@gmail.com>
2025-03-17 21:33:21 -0400

    remove prompt method that doesn't handle default values

--- functions/env_file.bash
+++ functions/env_file.bash
@@ -52,7 +52,7 @@ function set_env_file_var_value_if_empty() {
 
 # $1 = env file
 # $2 = var
-# $3 = info (optional)
+# $3 = default_value (optional)
 function prompt_and_set_env_file_var_value() {
   check_at_least_2_args "$@"
   check_at_most_3_args "$@"
@@ -60,7 +60,7 @@ function prompt_and_set_env_file_var_value() {
   assert_env_file_var_exists "$1" "$2"
   local var_value
   if [[ -n "$3" ]]; then
-    var_value="$(prompt_for_value "Enter value for $2 ($3)")" || exit 1
+    var_value="$(prompt_for_value "Enter value for $2" "$3")" || exit 1
   else
     var_value="$(prompt_for_value "Enter value for $2")" || exit 1
   fi
@@ -69,37 +69,13 @@ function prompt_and_set_env_file_var_value() {
 
 # $1 = env file
 # $2 = var
-# $3 = info (optional)
+# $3 = default_value (optional)
 function prompt_and_set_env_file_var_value_if_empty() {
   check_at_least_2_args "$@"
   check_at_most_3_args "$@"
   assert_file_exists "$1"
   assert_env_file_var_exists "$1" "$2"
   if is_env_file_var_value_empty "$1" "$2"; then
-    prompt_and_set_env_file_var_value "$1" "$2" "${3:-}"
-  fi
-}
-
-# $1 = env file
-# $2 = var
-# $3 = default_value
-function prompt_with_default_and_set_env_file_var_value() {
-  check_exactly_3_args "$@"
-  assert_file_exists "$1"
-  assert_env_file_var_exists "$1" "$2"
-  local var_value
-  var_value="$(prompt_for_value "Enter value for $2" "$3")" || exit 1
-  set_env_file_var_value "$1" "$2" "${var_value}"
-}
-
-# $1 = env file
-# $2 = var
-# $3 = default_value
-function prompt_with_default_and_set_env_file_var_value_if_empty() {
-  check_exactly_3_args "$@"
-  assert_file_exists "$1"
-  assert_env_file_var_exists "$1" "$2"
-  if is_env_file_var_value_empty "$1" "$2"; then
-    prompt_with_default_and_set_env_file_var_value "$1" "$2" "$3"
+    prompt_and_set_env_file_var_value "$1" "$2" "$3"
   fi
 }
