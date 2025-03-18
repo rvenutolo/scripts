e1efdf17310270ef2bf09012aea5f8ef327bafa8
Rick Venutolo <rick@venutolo.org>
2025-03-17 22:47:27 -0400

    rework of prompt stuff

--- functions/env_file.bash
+++ functions/env_file.bash
@@ -52,54 +52,146 @@ function set_env_file_var_value_if_empty() {
 
 # $1 = env file
 # $2 = var
-# $3 = info (optional)
-function prompt_and_set_env_file_var_value() {
+# $3 = var info (optional)
+# $4 = default value (optional)
+function prompt_env_file_var_value() {
   check_at_least_2_args "$@"
-  check_at_most_3_args "$@"
+    check_at_most_4_args "$@"
   assert_file_exists "$1"
   assert_env_file_var_exists "$1" "$2"
-  local var_value
-  if [[ -n "$3" ]]; then
-    var_value="$(prompt_for_value "Enter value for $2 ($3)")" || exit 1
+  local prompt_text
+  if [[ -n "${3:-}" ]]; then
+    prompt_text="Enter value for $2 ($3)"
   else
-    var_value="$(prompt_for_value "Enter value for $2")" || exit 1
+    prompt_text="Enter value for $2"
+  fi
+  local var_value
+  if [[ -n "${4:-}" ]]; then
+    var_value="$(prompt_for_value "${prompt_text}" "$4")" || exit 1
+  else
+    var_value="$(prompt_for_value "${prompt_text}")" || exit 1
   fi
   set_env_file_var_value "$1" "$2" "${var_value}"
 }
 
 # $1 = env file
 # $2 = var
-# $3 = info (optional)
-function prompt_and_set_env_file_var_value_if_empty() {
+# $3 = var info (optional)
+# $4 = default value (optional)
+function prompt_env_file_var_value_if_empty() {
+  check_at_least_2_args "$@"
+  check_at_most_4_args "$@"
+  assert_file_exists "$1"
+  assert_env_file_var_exists "$1" "$2"
+  if is_env_file_var_value_empty "$1" "$2"; then
+    prompt_env_file_var_value "$1" "$2" "${3:-}" "${4:-}"
+  fi
+}
+
+# $1 = env file
+# $2 = var
+# $3 = default value
+function prompt_env_file_value_with_default() {
+  check_exactly_3_args "$@"
+  assert_file_exists "$1"
+  assert_env_file_var_exists "$1" "$2"
+  prompt_env_file_var_value "$1" "$2" '' "$3"
+}
+
+# $1 = env file
+# $2 = var
+# $3 = default value
+function prompt_env_file_value_with_default_if_empty() {
+  check_exactly_3_args "$@"
+  assert_file_exists "$1"
+  assert_env_file_var_exists "$1" "$2"
+  prompt_env_file_var_value "$1" "$2" '' "$3"
+  if is_env_file_var_value_empty "$1" "$2"; then
+    prompt_env_file_value_with_default "$1" "$2" "${3:-}"
+  fi
+}
+
+# $1 = env file
+# $2 = var
+# $3 = var info (optional)
+function prompt_env_file_pw_value() {
+  check_at_least_2_args "$@"
+  check_at_most_3_args "$@"
+  assert_file_exists "$1"
+  assert_env_file_var_exists "$1" "$2"
+  prompt_env_file_var_value "$1" "$2" "${3:-}" "$(generate_password)"
+}
+
+# $1 = env file
+# $2 = var
+# $3 = var info (optional)
+function prompt_env_file_pw_value_if_empty() {
   check_at_least_2_args "$@"
   check_at_most_3_args "$@"
   assert_file_exists "$1"
   assert_env_file_var_exists "$1" "$2"
   if is_env_file_var_value_empty "$1" "$2"; then
-    prompt_and_set_env_file_var_value "$1" "$2" "${3:-}"
+    prompt_env_file_pw_value "$1" "$2" "${3:-}"
   fi
 }
 
-# $1 = env file
-# $2 = var
-# $3 = default_value
-function prompt_with_default_and_set_env_file_var_value() {
-  check_exactly_3_args "$@"
-  assert_file_exists "$1"
-  assert_env_file_var_exists "$1" "$2"
-  local var_value
-  var_value="$(prompt_for_value "Enter value for $2" "$3")" || exit 1
-  set_env_file_var_value "$1" "$2" "${var_value}"
-}
 
-# $1 = env file
-# $2 = var
-# $3 = default_value
-function prompt_with_default_and_set_env_file_var_value_if_empty() {
-  check_exactly_3_args "$@"
-  assert_file_exists "$1"
-  assert_env_file_var_exists "$1" "$2"
-  if is_env_file_var_value_empty "$1" "$2"; then
-    prompt_with_default_and_set_env_file_var_value "$1" "$2" "$3"
-  fi
-}
+
+
+
+
+
+
+## $1 = env file
+## $2 = var
+## $3 = info (optional)
+#function prompt_and_set_env_file_var_value() {
+#  check_at_least_2_args "$@"
+#  check_at_most_3_args "$@"
+#  assert_file_exists "$1"
+#  assert_env_file_var_exists "$1" "$2"
+#  local var_value
+#  if [[ -n "$3" ]]; then
+#    var_value="$(prompt_for_value "Enter value for $2 ($3)")" || exit 1
+#  else
+#    var_value="$(prompt_for_value "Enter value for $2")" || exit 1
+#  fi
+#  set_env_file_var_value "$1" "$2" "${var_value}"
+#}
+#
+## $1 = env file
+## $2 = var
+## $3 = info (optional)
+#function prompt_and_set_env_file_var_value_if_empty() {
+#  check_at_least_2_args "$@"
+#  check_at_most_3_args "$@"
+#  assert_file_exists "$1"
+#  assert_env_file_var_exists "$1" "$2"
+#  if is_env_file_var_value_empty "$1" "$2"; then
+#    prompt_and_set_env_file_var_value "$1" "$2" "${3:-}"
+#  fi
+#}
+#
+## $1 = env file
+## $2 = var
+## $3 = default_value
+#function prompt_with_default_and_set_env_file_var_value() {
+#  check_exactly_3_args "$@"
+#  assert_file_exists "$1"
+#  assert_env_file_var_exists "$1" "$2"
+#  local var_value
+#  var_value="$(prompt_for_value "Enter value for $2" "$3")" || exit 1
+#  set_env_file_var_value "$1" "$2" "${var_value}"
+#}
+#
+## $1 = env file
+## $2 = var
+## $3 = default_value
+#function prompt_with_default_and_set_env_file_var_value_if_empty() {
+#  check_exactly_3_args "$@"
+#  assert_file_exists "$1"
+#  assert_env_file_var_exists "$1" "$2"
+#  if is_env_file_var_value_empty "$1" "$2"; then
+#    prompt_with_default_and_set_env_file_var_value "$1" "$2" "$3"
+#  fi
+#}
