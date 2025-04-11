9819029334583e55e474d58feaa7d14f067f6551
Rick Venutolo <rick.venutolo@gmail.com>
2025-04-11 18:47:35 -0400

    add create_docker_network

--- functions/docker.bash
+++ functions/docker.bash
@@ -5,3 +5,13 @@ function container_is_running() {
   check_exactly_1_arg "$@"
   [[ -n "$(docker ps --quiet --filter "name=^$1\$")" ]] && [[ "$(docker container inspect -f '{{.State.Status}}' "$1")" == 'running' ]]
 }
+
+# $1 = docker network name
+function create_docker_network() {
+  check_exactly_1_arg "$@"
+  if ! docker network inspect "$1" &> '/dev/null'; then
+    log "Creating $1 network"
+    docker network create "$1"
+    log "Created $1 network"
+  fi
+}
