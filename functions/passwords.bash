761bb2282fc95826cb42bfa67df23f16a1b21735
Rick Venutolo <rick.venutolo@gmail.com>
2025-04-15 18:02:11 -0400

    longer passwords

--- functions/passwords.bash
+++ functions/passwords.bash
@@ -3,11 +3,11 @@
 # $1 = length (optional)
 function generate_password() {
   check_at_most_1_arg "$@"
-  pwgen --secure --capitalize --numerals --num-passwords 1 "${1:-32}"
+  pwgen --secure --capitalize --numerals --num-passwords 1 "${1:-64}"
 }
 
 # $1 = length (optional)
 function generate_password_with_symbols() {
   check_at_most_1_arg "$@"
-  pwgen --secure --capitalize --numerals --symbols --remove-chars '!&*{}[],#>|@`"%-.$\\/:;='\' --num-passwords 1 "${1:-32}"
+  pwgen --secure --capitalize --numerals --symbols --remove-chars '!&*{}[],#>|@`"%-.$\\/:;='\' --num-passwords 1 "${1:-64}"
 }
