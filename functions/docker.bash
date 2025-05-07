faa9e3146d8fc5389005165459e9f5db3cffec11
Rick Venutolo <rick@venutolo.org>
2025-05-07 19:34:11 -0400

    add wait_for_healthy_container

--- functions/docker.bash
+++ functions/docker.bash
@@ -6,6 +6,14 @@ function container_is_running() {
   [[ -n "$(docker ps --quiet --filter "name=^$1\$")" ]] && [[ "$(docker container inspect -f '{{.State.Status}}' "$1")" == 'running' ]]
 }
 
+# $1 = container name
+function wait_for_healthy_container() {
+  log "Waiting for $1 to be healthy"
+  until [[ "$(docker inspect -f '{{.State.Health.Status}}' $1)" == 'healthy' ]]; do
+      sleep 0.1
+  done
+}
+
 # $1 = docker network name
 function create_docker_network() {
   check_exactly_1_arg "$@"
