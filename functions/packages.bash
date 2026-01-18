dcfe4a18e9c0b768ea490aba04cad64cdbf6d464
Rick Venutolo <rick@venutolo.org>
2026-01-17 23:43:23 -0500

    update package functions to use --quiet and --ignore

--- functions/packages.bash
+++ functions/packages.bash
@@ -6,6 +6,8 @@ function dpkg_package_installed() {
 }
 
 # $1 = packages list type (appimage flatpak nixpkgs)
+# --quiet = don't output messages about disabled packages
+# --ignore [PACKAGE]... ignores those packages
 function get_universal_packages() {
   check_at_least_1_arg "$@"
   local package_type
@@ -73,11 +75,48 @@ function get_universal_packages() {
 
 # $1 = id
 # $2 = codename
+# --quiet = don't output messages about disabled packages
+# --ignore [PACKAGE]... ignores those packages
 function get_distro_packages() {
-  check_exactly_2_args "$@"
-  local package_list_url="https://raw.githubusercontent.com/rvenutolo/packages/main/$1-$2.csv"
+  check_at_least_2_arg "$@"
+  local id="$1"
+  shift
+  local codename="$1"
+  shift
+  local packages_to_ignore=()
+  local quiet=''
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --ignore)
+        if [[ $# -lt 2 ]]; then
+          die "--ignore requires at least one argument"
+        fi
+        shift
+        while [[ $# -gt 0 ]] && [[ ! "$1" =~ ^- ]]; do
+          packages_to_ignore+=("$1")
+          shift
+        done
+        ;;
+      --quiet)
+        quiet=1
+        shift
+        ;;
+      -*)
+        die "Unexpected flag '$1'"
+        return 1
+        ;;
+      *)
+        # Any remaining non-flag args can be treated as extra inputs or error
+        die "Unexpected argument '$1'"
+        return 1
+        ;;
+    esac
+  done
+  readonly packages_to_ignore
+  readonly quiet
+  local package_list_url="https://raw.githubusercontent.com/rvenutolo/packages/main/${id}-${codename}.csv"
   if ! curl_wrapper --output '/dev/null' --head "${package_list_url}"; then
-    die "No packages list for $1 $2"
+    die "No packages list for ${id} ${codename}"
   fi
   if is_personal && is_desktop; then
     local package_list_column=2
@@ -90,10 +129,68 @@ function get_distro_packages() {
   else
     die 'Could not determine which computer this is'
   fi
-  local disabled_awk_string="\$${package_list_column} == \"y\" && \$6 != \"\" { print \"Disabled package: \" \$1 \" (\" \$6 \")\" }"
-  IFS=$'\n'
-  for pkg_info in $(download_and_cat "${package_list_url}" | awk -F ',' "${disabled_awk_string}"); do log "$pkg_info"; done
-  unset IFS
+  if [[ -z "${quiet}" ]]; then
+    local disabled_awk_string="\$${package_list_column} == \"y\" && \$6 != \"\" { print \"Disabled package: \" \$1 \" (\" \$6 \")\" }"
+    IFS=$'\n'
+    for pkg_info in $(download_and_cat "${package_list_url}" | awk -F ',' "${disabled_awk_string}"); do log "$pkg_info"; done
+    unset IFS
+  fi
   local enabled_awk_string="\$${package_list_column} == \"y\" && \$6 == \"\" { print \$1 }"
-  download_and_cat "${package_list_url}" | awk -F ',' "${enabled_awk_string}"
+  comm -23 <(download_and_cat "${package_list_url}" | awk -F ',' "${enabled_awk_string}" | sort) <(printf '%s\n' "${packages_to_ignore[@]}" | sort)
+}
+
+# --quiet = don't output messages about disabled packages
+# --ignore [PACKAGE]... ignores those packages
+function get_sdkman_packages() {
+  local packages_to_ignore=()
+  local quiet=''
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --ignore)
+        if [[ $# -lt 2 ]]; then
+          die "--ignore requires at least one argument"
+        fi
+        shift
+        while [[ $# -gt 0 ]] && [[ ! "$1" =~ ^- ]]; do
+          packages_to_ignore+=("$1")
+          shift
+        done
+        ;;
+      --quiet)
+        quiet=1
+        shift
+        ;;
+      -*)
+        die "Unexpected flag '$1'"
+        return 1
+        ;;
+      *)
+        # Any remaining non-flag args can be treated as extra inputs or error
+        die "Unexpected argument '$1'"
+        return 1
+        ;;
+    esac
+  done
+  readonly packages_to_ignore
+  readonly quiet
+  local package_list_url='https://raw.githubusercontent.com/rvenutolo/packages/main/sdkman.csv'
+  if is_personal && is_desktop; then
+    local package_list_column=3
+  elif is_personal && is_laptop; then
+    local package_list_column=4
+  elif is_work && is_laptop; then
+    local package_list_column=5
+  elif is_server; then
+    local package_list_column=6
+  else
+    die 'Could not determine which computer this is'
+  fi
+  if [[ -z "${quiet}" ]]; then
+    local disabled_awk_string="\$${package_list_column} == \"y\" && \$7 != \"\" { print \"Disabled package: \" \$2 \" (\" \$7 \")\" }"
+    IFS=$'\n'
+    for pkg_info in $(download_and_cat "${package_list_url}" | awk --field-separator ',' "${disabled_awk_string}"); do log "$pkg_info"; done
+    unset IFS
+  fi
+  local enabled_awk_string="\$${package_list_column} == \"y\" && \$7 == \"\" { print \$2 }"
+  comm -23 <(download_and_cat "${package_list_url}" | awk -F ',' "${enabled_awk_string}" | sort) <(printf '%s\n' "${packages_to_ignore[@]}" | sort)
 }
