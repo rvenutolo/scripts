b58b26aa9fe4118dfe713668a38549f6e68c0ef1
Rick Venutolo <rick@venutolo.org>
2026-01-17 19:12:40 -0500

    separate functions file for sdkman stuff

--- functions/packages.bash
+++ functions/packages.bash
@@ -5,28 +5,6 @@ function dpkg_package_installed() {
   dpkg-query --show --showformat='${Package};${Status}\n' "$1" 2> '/dev/null' | contains_regex_ignore_case "^$1;.*ok installed\$"
 }
 
-function get_sdkman_packages() {
-  check_no_args "$@"
-  local package_list_url='https://raw.githubusercontent.com/rvenutolo/packages/main/sdkman.csv'
-  if is_personal && is_desktop; then
-    local package_list_column=3
-  elif is_personal && is_laptop; then
-    local package_list_column=4
-  elif is_work && is_laptop; then
-    local package_list_column=5
-  elif is_server; then
-    local package_list_column=6
-  else
-    die 'Could not determine which computer this is'
-  fi
-  local disabled_awk_string="\$${package_list_column} == \"y\" && \$7 != \"\" { print \"Disabled package: \" \$2 \" (\" \$7 \")\" }"
-  IFS=$'\n'
-  for pkg_info in $(download_and_cat "${package_list_url}" | awk -F ',' "${disabled_awk_string}"); do log "$pkg_info"; done
-  unset IFS
-  local enabled_awk_string="\$${package_list_column} == \"y\" && \$7 == \"\" { print \$2 }"
-  download_and_cat "${package_list_url}" | awk -F ',' "${enabled_awk_string}"
-}
-
 # $1 = packages list type (appimage flatpak nixpkgs)
 function get_universal_packages() {
   check_at_least_1_arg "$@"
---
+++ functions/sdkman.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function get_sdkman_packages() {
+  check_no_args "$@"
+  local package_list_url='https://raw.githubusercontent.com/rvenutolo/packages/main/sdkman.csv'
+  if is_personal && is_desktop; then
+    local package_list_column=3
+  elif is_personal && is_laptop; then
+    local package_list_column=4
+  elif is_work && is_laptop; then
+    local package_list_column=5
+  elif is_server; then
+    local package_list_column=6
+  else
+    die 'Could not determine which computer this is'
+  fi
+  local disabled_awk_string="\$${package_list_column} == \"y\" && \$7 != \"\" { print \"Disabled package: \" \$2 \" (\" \$7 \")\" }"
+  IFS=$'\n'
+  for pkg_info in $(download_and_cat "${package_list_url}" | awk -F ',' "${disabled_awk_string}"); do log "$pkg_info"; done
+  unset IFS
+  local enabled_awk_string="\$${package_list_column} == \"y\" && \$7 == \"\" { print \$2 }"
+  download_and_cat "${package_list_url}" | awk -F ',' "${enabled_awk_string}"
+}
