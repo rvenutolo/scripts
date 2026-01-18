ba37a39f6f4b2d3d09add62c15e8b0fb8e430e96
Rick Venutolo <rick@venutolo.org>
2026-01-17 23:42:43 -0500

    add remove_ansi and remove_empty_lines

--- functions/strings.bash
+++ functions/strings.bash
@@ -12,3 +12,17 @@ function ensure_trailing_slash() {
     echo "$1"
   fi
 }
+
+# expected to pipe to this function: ex my_command | remove_ansi
+function remove_ansi() {
+  check_no_args "$@"
+  check_for_stdin
+  sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"
+}
+
+# expected to pipe to this function, ex: my_command | remove_empty_lines
+function remove_empty_lines() {
+  check_no_args "$@"
+  check_for_stdin
+  sed '/^[[:space:]]*$/d'
+}
