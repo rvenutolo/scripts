6bcffac538d5edded8a6296d90191e73ed8c7937
Rick Venutolo <rick.venutolo@gmail.com>
2026-01-18 22:39:00 -0500

    shellcheck fixes

--- functions/sdkman.bash
+++ functions/sdkman.bash
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+#shellcheck disable=SC2120
 function clean_sdkman_output() {
   check_no_args "$@"
   check_for_stdin
   remove_ansi | remove_empty_lines
 }
 
+#shellcheck disable=SC2120
 function update_sdkman_metadata() {
   check_no_args "$@"
   sdk update | clean_sdkman_output
