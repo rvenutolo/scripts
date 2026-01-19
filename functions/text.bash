2d48dd30bace5440996944e5d06a8dabca38a98d
Rick Venutolo <rick.venutolo@gmail.com>
2026-01-19 00:10:48 -0500

    move some functions to text.sh

---
+++ functions/text.bash
@@ -0,0 +1,13 @@
+# expected to pipe to this function: ex my_command | remove_ansi
+function remove_ansi() {
+  check_no_args "$@"
+  check_for_stdin
+  sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"
+}
+
+# expected to pipe to this function, ex: my_command | remove_empty_lines
+function remove_empty_lines() {
+  check_no_args "$@"
+  check_for_stdin
+  sed '/^[[:space:]]*$/d'
+}
