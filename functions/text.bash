c517e8b614cd4965b68eefab44425aebb19f2d74
Rick Venutolo <rick@venutolo.org>
2026-01-19 22:19:38 -0500

    use descriptive flag

--- functions/text.bash
+++ functions/text.bash
@@ -4,7 +4,7 @@
 function remove_ansi() {
   check_no_args "$@"
   check_for_stdin
-  sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"
+  sed --regexp-extended "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g"
 }
 
 # expected to pipe to this function, ex: my_command | remove_empty_lines
