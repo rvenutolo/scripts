71fb1625745382e531f668333817399c43aa8f77
Rick Venutolo <rick@venutolo.org>
2026-05-05 16:08:55 -0400

    move some functions

--- functions/files.bash
+++ functions/files.bash
@@ -27,53 +27,6 @@ function file_size_gb() {
   echo "scale=2; $(stat --format='%s' "$1") / 1073741824" | bc
 }
 
-# $1 = target file
-# $2 = link file
-function link_file() {
-  check_exactly_2_args "$@"
-  assert_file_exists "$1"
-  if [[ -L "$2" && "$(readlink --canonicalize "$2")" == "$(readlink --canonicalize "$1")" ]]; then
-    return 0
-  fi
-  if file_exists "$2"; then
-    diff --color --unified "$2" "$1" || true
-    if ! prompt_yn "$2 exists - Link: $1 -> $2?"; then
-      return 0
-    fi
-  else
-    if ! prompt_yn "Link: $1 -> $2?"; then
-      return 0
-    fi
-  fi
-  log "Linking: $1 -> $2"
-  create_dir "$(dirname "$2")"
-  ln --symbolic --force "$1" "$2"
-  log "Linked: $1 -> $2"
-}
-
-# $1 = target directory
-# $2 = link path
-function link_dir() {
-  check_exactly_2_args "$@"
-  assert_dir_exists "$1"
-  if [[ -L "$2" && "$(readlink --canonicalize "$2")" == "$(readlink --canonicalize "$1")" ]]; then
-    return 0
-  fi
-  if dir_exists "$2"; then
-    if ! prompt_yn "$2 exists - Link: $1 -> $2?"; then
-      return 0
-    fi
-  else
-    if ! prompt_yn "Link: $1 -> $2?"; then
-      return 0
-    fi
-  fi
-  log "Linking: $1 -> $2"
-  create_dir "$(dirname "$2")"
-  ln --symbolic --force "$1" "$2"
-  log "Linked: $1 -> $2"
-}
-
 # $1 = old file location
 # $2 = new file location
 function move_file() {
