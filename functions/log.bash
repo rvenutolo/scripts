a19a3d701f932880cf38ccc74415daa13f7b9ac5
Rick Venutolo <rick.venutolo@gmail.com>
2026-05-05 00:29:14 -0400

    enforce shell-script style rules across repo
      Apply global shell-script conventions to 32 scripts: prefer long CLI
      options over short flags, replace `for x in $(cmd)` with `mapfile -t`,
      swap `echo -e`/`echo -n` for `printf`, add `--` separators to
      destructive commands, use brace-syntax variable expansions, quote
      command substitutions, and add EXIT traps for tempfile cleanup.

--- functions/log.bash
+++ functions/log.bash
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 function log() {
-  echo -e "\033[0;32m[$(date +%T) ${0##*/}] $*\033[0m" >&2
+  printf '\033[0;32m[%s %s] %s\033[0m\n' "$(date +%T)" "${0##*/}" "$*" >&2
 }
 
 function log_with_date() {
-  echo -e "\033[0;32m[$(date '+%Y-%m-%d %T') ${0##*/}] $*\033[0m" >&2
+  printf '\033[0;32m[%s %s] %s\033[0m\n' "$(date '+%Y-%m-%d %T')" "${0##*/}" "$*" >&2
 }
 
 function die() {
-  echo -e "\033[0;31mDIE: $* (at ${BASH_SOURCE[1]}:${FUNCNAME[1]} line ${BASH_LINENO[0]})\033[0m" >&2
+  printf '\033[0;31mDIE: %s (at %s:%s line %s)\033[0m\n' "$*" "${BASH_SOURCE[1]}" "${FUNCNAME[1]}" "${BASH_LINENO[0]}" >&2
   exit 1
 }
