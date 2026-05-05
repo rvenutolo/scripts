1e260aa2d06704e7b7cbce8c51f5986448846f57
Rick Venutolo <rick.venutolo@gmail.com>
2026-05-05 00:34:52 -0400

    remove is_pop, is_endeavour, and is_manjaro functions

--- functions/os.bash
+++ functions/os.bash
@@ -25,18 +25,6 @@ function is_arch() {
 }
 
 #shellcheck disable=SC2120
-function is_manjaro() {
-  check_no_args "$@"
-  [[ "$(os_id)" == 'manjaro' ]]
-}
-
-#shellcheck disable=SC2120
-function is_endeavour() {
-  check_no_args "$@"
-  [[ "$(os_id)" == 'endeavouros' ]]
-}
-
-#shellcheck disable=SC2120
 function is_fedora() {
   check_no_args "$@"
   [[ "$(os_id)" == 'fedora' ]]
@@ -52,12 +40,6 @@ function is_debian() {
 function is_ubuntu() {
   check_no_args "$@"
   [[ "$(os_id)" == 'ubuntu' ]]
-}
-
-#shellcheck disable=SC2120
-function is_pop() {
-  check_no_args "$@"
-  [[ "$(os_id)" == 'pop' ]]
 }
 
 #shellcheck disable=SC2120
