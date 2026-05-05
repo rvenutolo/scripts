f9d99d40abd57a1370295d5b532868ebf3c80044
Rick Venutolo <rick.venutolo@gmail.com>
2026-05-05 16:20:31 -0400

    add log_warn func

--- functions/log.bash
+++ functions/log.bash
@@ -8,6 +8,10 @@ function log_with_date() {
   printf '\033[0;32m[%s %s] %s\033[0m\n' "$(date '+%Y-%m-%d %T')" "${0##*/}" "$*" >&2
 }
 
+function log_warn() {
+  printf '\033[0;33m[%s %s] WARN: %s\033[0m\n' "$(date +%T)" "${0##*/}" "$*" >&2
+}
+
 function die() {
   printf '\033[0;31mDIE: %s (at %s:%s line %s)\033[0m\n' "$*" "${BASH_SOURCE[1]}" "${FUNCNAME[1]}" "${BASH_LINENO[0]}" >&2
   exit 1
